c832bd331e4130a60d6897285f00dcfb3f9e29b1
radittaza <radityaazanata028@gmail.com>
2024-12-26 21:58:55 +0700

    Menambahkan file yang gagal

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
           </Link>
           <Link href="/register"
             className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Register
+            Register Account
           </Link>
         </div>
       </div>
